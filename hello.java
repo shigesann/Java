5cdc20d5282db09d5c1a04d3181f76ef0c670d16
shigesan <gydpc105@sky.bbexcite.jp>
2020-10-01 23:12:07 +0900

    branch-A is commited.

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
-		System.out.println("branch-A branch.");
-	}
+		System.out.println("branch-A AnotherDirectory.");
+	}	
 }
